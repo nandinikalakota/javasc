62613a366719e6c3245c49cf453a9d799731db7e
Nandini <kalakotanandini@gmail.com>
2023-08-22 16:05:54 +1000

    portfolio v1 done

---
+++ script.js
@@ -0,0 +1,14 @@
+// single line Comment
+// console,log("gug");
+// alert("hi");
+// ==variables
+// var ,let, const
+// var firstName;creating variable
+var firstName = "nandini kalakota";
+let lastName = "nandini";
+const dob = "08-11-2000";
+console.log(firstName);
+firstName = "sai";
+lastName = "raj";
+dob = "15-6-1996";
+console.log(firstName, lastName, dob);
